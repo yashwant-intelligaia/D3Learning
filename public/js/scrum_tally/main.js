17dd2c445320ebcdda5ddcbd647c76d578e64f9e
Yashwant Rautela <yashwant.rautela@intelligaia.com>
2020-05-18 16:58:42 +0530

    labels added at scrum tally graph

--- public/js/scrum_tally/main.js
+++ public/js/scrum_tally/main.js
@@ -1,5 +1,5 @@
 // set the dimensions and margins of the graph
-var margin = {top: 20, right: 30, bottom: 40, left: 90},
+var margin = {top: 20, right: 30, bottom: 40, left: 120},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
@@ -9,6 +9,7 @@ var svg = d3.select("#my_dataviz")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
+    .attr("class", "main-group")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
@@ -16,23 +17,42 @@ var svg = d3.select("#my_dataviz")
 d3.csv("/asset/csv/scrum_tally.csv", function(data) {
   // Add X axis
   var x = d3.scaleLinear()
-    // .domain([0, 50])
-    .domain(d3.extent(data, function(d){return parseInt(d.Value)}))
+    // .domain(d3.extent(data, function(d){return parseInt(d.Value)}))
+    .domain([d3.min(data, function(d){return parseInt(d.Value)}),d3.max(data, function(d){return parseInt(d.Value)})])
     .range([ 0, width]);
+
   svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
+    .attr("transform", "translate(0," + (height-50) + ")")
     .call(d3.axisBottom(x))
     .selectAll("text")
-      .attr("transform", "translate(-10,0)rotate(-45)")
-      .style("text-anchor", "end");
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end");
+
+  d3.select("svg")
+    .select("g")
+    .append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .append("text")
+    .attr("x", width/2)
+    .attr("stroke", "black")
+    .attr("font-size", "14px")
+    .text("Year");
 
   // Y axis
   var y = d3.scaleBand()
-    .range([ 0, height ])
+    .range([ 0, height-50 ])
     .domain(data.map(function(d) { return d.Scrum_Master; }))
     .padding(.1);
   svg.append("g")
     .call(d3.axisLeft(y))
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("x", -((height-50)/2))
+    .attr("dy", "-8.1em")
+    .attr("font-size", "14px")
+    .attr("stroke", "black")
+    .text("Members");
 
   //Bars
   svg.selectAll("myRect")
